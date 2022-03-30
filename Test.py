f651f794e9979639a0cae476982e8d1e2c5a0f34
duckhoan.hcmut <duckhoan.work@gmail.com>
2022-03-30 13:36:08 +0700

    first commit

---
+++ Test.py
@@ -0,0 +1,19 @@
+import glob
+import os
+
+# list_names = glob.glob('DataViDok/*.sdf')
+# list_names_2 = glob.glob('complex_ligand/*.sdf')
+# list_names_2 = [s.split('/')[-1] for s in list_names_2]
+# count =0
+# for name in list_names:
+#     n = name.split('/')[-1]
+#     if n not in list_names_2:
+#         print(name)
+#         # if os.path.exists(name):
+#         #     os.remove(name)
+
+# print(len(list_names))
+# print(len(list_names_2))
+
+
+print(os.getcwd())
